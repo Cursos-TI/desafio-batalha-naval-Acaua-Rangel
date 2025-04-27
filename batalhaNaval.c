c680f93e115730507fbbc40e01a0247e092ec1fd
Acauã Rangel <71088524+Acaua-Rangel@users.noreply.github.com>
2025-04-27 09:27:55 -0300

    Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -18,11 +18,13 @@ int podeAdicionarDiagonal(int x, int y) {
 
 int main() {
     int tabuleiro[LINHAS][COLUNAS];
+    int tabuleiro2[LINHAS][COLUNAS];
 
     // Preenchendo a matriz com zeros
     for (int l = 0; l < LINHAS; l++) {
         for (int a = 0; a < COLUNAS; a++) {
             tabuleiro[l][a] = 0;
+            tabuleiro2[l][a] = 0;
         }
     }
 
@@ -69,11 +71,54 @@ int main() {
         return 0;
     }
 
+
+    // ================= Geometria =================
+
+    // Cone
+
+    int cone_x = 3, cone_y = 6;
+
+    for (int c = 2, tam = 5, p = 5; c >= 0; c--) {
+        for (int h = 0; h < p; h++) {
+            int padding = (tam-p)/2;
+            tabuleiro[c+cone_y][padding+h+cone_x] = 1;
+        }
+        p-=2;
+    }
+
+    // Cruz
+
+    int cruz_x = 2, cruz_y = 3;
+
+    for (int c = 2, tam = 5; c >= 0; c--) {
+        if (c==1) {
+            for (int h = 0; h < tam; h++) {
+                tabuleiro[c+cruz_y][h+cruz_x] = 1;
+            }
+        } else {
+            tabuleiro[c+cruz_y][2+cruz_x] = 1;
+        }
+    }
+
+    // Octaedro
+
+    int octa_x = 0, octa_y = 0;
+
+    for (int c = 2, tam = 5; c >= 0; c--) {
+        if (c==1) {
+            for (int h = 0; h < tam-2; h++) {
+                tabuleiro[c+octa_y][h+octa_x+1] = 1;
+            }
+        } else {
+            tabuleiro[c+octa_y][2+octa_x] = 1;
+        }
+    }
+
     // Mostrando a matriz no console
     printf("\nTabuleiro:\n");
     for (int l = 0; l < LINHAS; l++) {
         for (int a = 0; a < COLUNAS; a++) {
-            printf("%d ", tabuleiro[l][a]);
+            printf("%d  ", tabuleiro[l][a]);
         }
         printf("\n");
     }
